2bf7564ec3ad283fb94b7b4155eb4e91ac61f91b
Mark Kerzner <mark@elephantscale.com>
2023-10-03 11:02:13 -0500

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # MosesAI
+
+* To prepare your environment
+
+```shell
+conda create --name MosesAI python=3.9
+```
---
+++ deploy_fastapi.sh
@@ -0,0 +1,2 @@
+# Start a fastapi application
+sudo /home/ubuntu/anaconda3/bin/uvicorn  main:app --host 0.0.0.0 --port 80
